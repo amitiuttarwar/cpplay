5bfd2d58e20259d2d289116876a528b7657728ff
Amiti Uttarwar <amiti@uttarwar.org>
2021-02-22 17:12:28 -0800

    Net Permission Flag enum understanding

---
+++ net_permissions.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bitset>
+
+enum NetPermissionFlags {
+    PF_NONE = 0,
+    // Can query bloomfilter even if -peerbloomfilters is false
+    PF_BLOOMFILTER = (1U << 1),
+    // Relay and accept transactions from this peer, even if -blocksonly is true
+    // This peer is also not subject to limits on how many transaction INVs are tracked
+    PF_RELAY = (1U << 3),
+    // Always relay transactions from this peer, even if already in mempool
+    // Keep parameter interaction: forcerelay implies relay
+    PF_FORCERELAY = (1U << 2) | PF_RELAY,
+    // Allow getheaders during IBD and block-download after maxuploadtarget limit
+    PF_DOWNLOAD = (1U << 6),
+    // Can't be banned/disconnected/discouraged for misbehavior
+    PF_NOBAN = (1U << 4) | PF_DOWNLOAD,
+    // Can query the mempool
+    PF_MEMPOOL = (1U << 5),
+    // Can request addrs without hitting a privacy-preserving cache
+    PF_ADDR = (1U << 7),
+
+    // True if the user did not specifically set fine grained permissions
+    PF_ISIMPLICIT = (1U << 31),
+    PF_ALL = PF_BLOOMFILTER | PF_FORCERELAY | PF_RELAY | PF_NOBAN | PF_MEMPOOL | PF_DOWNLOAD | PF_ADDR,
+};
+
+
+int main()
+{
+    enum NetPermissionFlags flags(PF_ALL);
+    std::cout << flags << std::endl;
+
+    std::bitset<8> x(flags);
+    std::cout << x << '\n';
+
+    return 0;
+}
+
+// 00000010, 2:   PF_BLOOMFILTER
+// 00001000, 8:   PF_RELAY
+// 00001100, 12:  PF_FORCERELAY (ors PF_RELAY)
+// 01000000, 64:  PF_DOWNLOAD
+// 01010000, 80:  PF_NOBAN (ors PF_DOWNLOAD)
+// 00100000, 32:  PF_MEMPOOL
+// 10000000, 128: PF_ADDR
+// 11111110, 254: PF_ALL
+
+// 00000000, 2147483648: PF_ISIMPLICIT
