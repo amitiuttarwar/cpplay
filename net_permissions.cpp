cabecdebcacc963427a49294ddfff8359f532f3a
Amiti Uttarwar <amiti@uttarwar.org>
2021-02-23 17:02:45 -0800

    moar permish

--- net_permissions.cpp
+++ net_permissions.cpp
@@ -41,11 +41,50 @@ static bool HasFlag(const NetPermissionFlags& flags, NetPermissionFlags f)
     return combined == f;
 }
 
+enum ServiceFlags : uint64_t {
+    NODE_NONE = 0,
+    // NODE_NETWORK means that the node is capable of serving the complete block chain. It is currently
+    // set by all Bitcoin Core non pruned nodes, and is unset by SPV clients or other light clients.
+    NODE_NETWORK = (1 << 0),
+    // NODE_BLOOM means the node is capable and willing to handle bloom-filtered connections.
+    // Bitcoin Core nodes used to support this by default, without advertising this bit,
+    // but no longer do as of protocol version 70011 (= NO_BLOOM_VERSION)
+    NODE_BLOOM = (1 << 2),
+    // NODE_WITNESS indicates that a node can be asked for blocks and transactions including
+    // witness data.
+    NODE_WITNESS = (1 << 3),
+    // NODE_COMPACT_FILTERS means the node will service basic block filter requests.
+    // See BIP157 and BIP158 for details on how this is implemented.
+    NODE_COMPACT_FILTERS = (1 << 6),
+    // NODE_NETWORK_LIMITED means the same as NODE_NETWORK with the limitation of only
+    // serving the last 288 (2 day) blocks
+    // See BIP159 for details on how this is implemented.
+    NODE_NETWORK_LIMITED = (1 << 10),
+};
+
+
 int main()
 {
-    enum NetPermissionFlags forcerelay(PF_FORCERELAY);
+    // see how the bitwise operations hits true for a subset of permissions
+    //enum NetPermissionFlags forcerelay(PF_FORCERELAY);
+    //std::cout << HasFlag(forcerelay, NetPermissionFlags::PF_RELAY) << std::endl;
 
-    std::cout << HasFlag(forcerelay, NetPermissionFlags::PF_FORCERELAY) << std::endl;
+    // how does & operator work here?
+    ServiceFlags nLocalServices = NODE_BLOOM;
+    std::bitset<8> z(nLocalServices);
+    std::cout << z << ": nLocalServices" << std::endl;
+
+    ServiceFlags bloom = NODE_BLOOM;
+    std::bitset<8> x(bloom);
+    std::cout << x << ": NODE_BLOOM" << std::endl;
+
+    auto combined = nLocalServices & bloom;
+    std::bitset<8> y(combined);
+    std::cout << y << ": combined" << std::endl;
+
+    if (!(nLocalServices & NODE_BLOOM)) {
+        std::cout << "disconnect" << std::endl;
+    }
 
     return 0;
 }
